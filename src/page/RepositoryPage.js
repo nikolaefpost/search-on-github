a554f977895760c3e357f94d10683e00756f7690
38093 <nikolaef.post@gmail.com>
2021-08-16 18:36:21 +0300

    fix error update RepositoryPage

--- src/page/RepositoryPage.js
+++ src/page/RepositoryPage.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import {Card, ListGroup, ListGroupItem} from "react-bootstrap";
 import {useSelector} from "react-redux";
-import { useParams} from 'react-router-dom'
+import { useParams, useHistory} from 'react-router-dom'
+import {SEARCH_ROUTE} from "../utils/consts";
+
 
 const RepositoryPage = () => {
 
+    const history = useHistory();
     const reposList = useSelector(state => state.repository.list);
+    if (reposList.length<1) history.push(SEARCH_ROUTE)
+
     const {id} = useParams();
     const repository = reposList.filter(item => item.id === Number(id))[0];
 
@@ -13,7 +18,7 @@ const RepositoryPage = () => {
 
     return (
         <div className='d-flex justify-content-center  ' style={{marginTop: "50px", width: '90%', marginLeft: 'auto', marginRight: 'auto'}}>
-            <Card  className='shadow '>
+            {(reposList.length>1)&&<Card className='shadow '>
                 <Card.Img className='p-2' style={{width: '500'}} variant="top" src={repository.owner.avatar_url}/>
                 <Card.Body>
                     <Card.Title>Login: {repository.owner.login}</Card.Title>
@@ -27,10 +32,11 @@ const RepositoryPage = () => {
                     <ListGroupItem>Language: {repository.language}</ListGroupItem>
                 </ListGroup>
                 <Card.Body>
-                    <Card.Link style={{cursor: 'pointer'}} onClick={()=>handleClick(repository.html_url)}>Repository Link</Card.Link>
+                    <Card.Link style={{cursor: 'pointer'}} onClick={() => handleClick(repository.html_url)}>Repository
+                        Link</Card.Link>
                 </Card.Body>
-            </Card>
-            
+            </Card>}
+
         </div>
     );
 };
